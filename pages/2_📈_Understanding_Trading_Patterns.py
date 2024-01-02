c96f68155d421653e6b47f70cb95014b1e73bb06
OOlajide <70548782+OOlajide@users.noreply.github.com>
2024-01-02 16:04:06 +0100

    Update 2_📈_Understanding_Trading_Patterns.py

--- pages/2_📈_Understanding_Trading_Patterns.py
+++ pages/2_📈_Understanding_Trading_Patterns.py
@@ -5,8 +5,9 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from millify import millify
 from streamlit_extras.colored_header import colored_header
+from urllib.request import Request, urlopen
 
-# st.cache_data.clear()
+st.cache_data.clear()
 
 st.set_page_config(
     page_title="Uniswap On L2s",
